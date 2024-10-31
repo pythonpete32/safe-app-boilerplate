e7d55b87f3196fb87a6704fed3731c5b568e9f7f
PythonPete32 <aaron@blackscale.io>
2024-10-31 20:41:31 +0000

    feat: :triangular_flag_on_post:

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,7 +1,9 @@
 const getSiteUrl = () => {
-	if (process.env.PRODUCTION_URL) return `https://${process.env.PRODUCTION_URL}`;
+	const url = import.meta.env.VITE_PRODUCTION_URL;
+	if (url) return `https://${import.meta.env.VITE_PRODUCTION_URL}`;
 	return "http://localhost:3000";
 };
 
 export const SITE_URL = getSiteUrl();
+console.log(SITE_URL);
 export const APP_NAME = "Safe App Boilerplate";
